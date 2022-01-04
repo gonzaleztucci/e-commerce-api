08c0da0c3014f6c43970853c7bd76663c464ab6f
gonzaleztucci <gonzaleztucci@gmail.com>
2022-01-04 19:49:03 +0100

    error handling added to register

--- server/register.js
+++ server/register.js
@@ -10,7 +10,7 @@ app.post('/', (req, res, next) => {
     pool.query('SELECT MAX(id) FROM users;', (err, result) => {
         console.log(req.body);
         if(err){
-            throw err
+            next(err);
         }
         else{
             const {max} = result.rows[0]; 
@@ -23,14 +23,14 @@ app.post('/', (req, res, next) => {
     // IF THERE´S ADDRESS INFO IN THE BODY, A NEW ADDRESS SHOULD BE CREATED
     (req, res, next) => {
         const {streetName, streetNumber, zipcode, city, country, apartmentNumber} = req.body;
-        
+
         if (streetName && streetNumber && zipcode && city && country) {
             console.log('TODO PARA CREAR ADDRESS')
             const text = 'INSERT INTO address (street_name, street_number, zipcode, city, country, apartment_number) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *;'
             const values = [streetName, streetNumber, zipcode, city, country, apartmentNumber]
             pool.query(text, values, (err, results) => {
                 if (err) {
-                    throw(err)
+                    next(err);
                 } else {
                     console.log(results.rows);
                 }
@@ -41,31 +41,31 @@ app.post('/', (req, res, next) => {
         }
         next();
     },
-
     (req, res, next) => {
         if(!req.body.userId){
             res.send('NO HAY USUARIOS'); //REVISAR
         } else {
             const text = 'INSERT INTO users (id, username, password, address_id, role_id, first_name, last_name, email, telephone_number) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *;';
-            console.log(req.body);
-            // const userName = req.body.userName;
-            // const password = req.body.password;
-            // const addressId = req.body.addressId;
-            // const roleId = req.body.roleId;
-            // const firstName = req.body.firstName;
-            // const last
             const {userId, userName, password, addressId, roleId, firstName, lastName, email, telephoneNumber} = req.body;
-            pool.query(text, [userId, userName, password, addressId, roleId, firstName, lastName, email, telephoneNumber], (err, result) => {
-                if (err){
-                    throw err
-                    res.sendStatus(404);
-                } else {
-                    res.json(result.rows);
-                }
-            })
+            if(userName && password && firstName && lastName && email) {
+                pool.query(text, [userId, userName, password, addressId, roleId, firstName, lastName, email, telephoneNumber], (err, result) => {
+                    if (err){
+                        next(err);
+                        
+                    } else {
+                        res.json(result.rows);
+                    }
+                })
 
+            } else {
+                console.log('Please fill the required fields')
+                res.status(500).send('Please fill the required fields')
+            }
 
         }
+    }, (err, req, res, next) => {
+        console.error(`Error: ${err}`);
+        res.status(500).send('Something broke!');
     })
     
     
