87b64979aabc8e9b486b9c91492e8e11832288e0
gonzaleztucci <gonzaleztucci@gmail.com>
2022-01-17 20:38:27 +0100

    update cart implemented

--- server/cart.js
+++ server/cart.js
@@ -4,21 +4,26 @@ const pool = require('../db/database');
 
 app.post('/', (req, res, next) => {
     const text = 'SELECT quantity FROM cart WHERE user_id = $1 AND product_id = $2';
-    const user_id = req.user_id;
-    const product_id = req.user_id;
+    const user_id = req.body.user_id;
+    const product_id = req.body.product_id;
 
     pool.query(text, [user_id, product_id], (err, result) => {
         if (err){
             throw err;
         } else {
-            req.cart_quantity = result.rows;
-            console.log(req.cart_quantity);
-            console.log('PRIMER METHOD');
+            if(result.rows.length > 0){
+                const {quantity} = result.rows[0];
+                req.body.cart_quantity = quantity;
+            } else {
+                req.body.cart_quantity = 0;
+            }
+                       
             next();
         }        
     })
 }, (req, res, next) => {
-    if (req.cart_quantity.length === 0) {
+    console.log(`CART QUANTITY: ${req.body.cart_quantity}`);
+    if (req.body.cart_quantity === 0) {
         const text = 'INSERT INTO cart (user_id, product_id, quantity, date_added) VALUES ($1, $2, $3, $4) RETURNING *;';
         const timestamp = new Date().toLocaleDateString();
         console.log(timestamp);
@@ -31,11 +36,13 @@ app.post('/', (req, res, next) => {
             }
         });
     } else {
-        const text = 'INSERT INTO cart (user_id, product_id, quantity, date_added) VALUES ($1, $2, $3, $4) RETURNING *;';
+        console.log('EN EL ELSE!!!!!!!!!!!');
+        const text = 'UPDATE cart SET quantity = $3, date_added = $4 WHERE user_id = $1 AND product_id = $2 RETURNING *;';
         const timestamp = new Date().toLocaleDateString();
         console.log(timestamp);
-        const quantity = req.cart_quantity[0] + parseInt(req.quantity, 10);
-        pool.query(text, [req.body.user_id, req.body.product_id, req.body.quantity, timestamp], (err, result) => {
+        const quantity = req.body.cart_quantity + req.body.quantity;
+        console.log(`QUANTITY   ${quantity}`);
+            pool.query(text, [req.body.user_id, req.body.product_id, quantity , timestamp], (err, result) => {
             if(err) {
                 throw err;
             } else {
