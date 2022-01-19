0e208f7d076ecc6fdd13db9500bd75d13c0f869a
gonzaleztucci <gonzaleztucci@gmail.com>
2022-01-19 20:52:57 +0100

    swagger initial configuration

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
 const express = require('express');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
+const swaggerUI = require('swagger-ui-express');
+const swaggerJsDoc = require('swagger-jsdoc');
+
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "e-commerce API",
+      version:"1.0.0",
+      description: "A simple e-commerce Express API"
+    },
+    servers: [
+      {
+        url: "http://localhost:3000"
+      }
+    ]
+  },
+  apis: ["./routes/*.js"]
+};
+
+const specs = swaggerJsDoc(options);
 
 
 const app = express();
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(specs));
+
 const pool = require('./db/database');
 const PORT = process.env.PORT || 3000;
-const apiRouter = require('./server/api');
+const apiRouter = require('./routes/api');
 
 app.use(express.json())//Nos da acceso al req.body
 app.use(passport.initialize());
